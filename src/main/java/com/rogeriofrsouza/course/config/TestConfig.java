7aee90967a07234cae9c61a7cdbb9885677a22d8
Rogério Ferreira de Souza <rogeriofrsouza@gmail.com>
2023-03-17 15:41:50 -0300

    Payment, one-to-one association

--- src/main/java/com/rogeriofrsouza/course/config/TestConfig.java
+++ src/main/java/com/rogeriofrsouza/course/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 import com.rogeriofrsouza.course.entities.Category;
 import com.rogeriofrsouza.course.entities.Order;
 import com.rogeriofrsouza.course.entities.OrderItem;
+import com.rogeriofrsouza.course.entities.Payment;
 import com.rogeriofrsouza.course.entities.Product;
 import com.rogeriofrsouza.course.entities.User;
 import com.rogeriofrsouza.course.entities.enums.OrderStatus;
@@ -86,6 +87,12 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		
+		// Salvando objeto dependente no banco de dados (relação um para um)
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
 	}
 	
 }
