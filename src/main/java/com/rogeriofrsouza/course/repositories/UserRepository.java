0df99b92b1c0224560ca85e5d4779c346af6a135
Rogério Ferreira de Souza <rogeriofrsouza@gmail.com>
2024-01-02 20:45:10 -0300

    style: removed comments

--- src/main/java/com/rogeriofrsouza/course/repositories/UserRepository.java
+++ src/main/java/com/rogeriofrsouza/course/repositories/UserRepository.java
@@ -4,10 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.rogeriofrsouza.course.entities.User;
 
-/*
- * Spring Data JPA possui uma implementação padrão desta interface
- * Não necessita do @Repository pois JpaRepository já está registrado como componente do Spring
- */
 public interface UserRepository extends JpaRepository<User, Integer> {
 
 }
